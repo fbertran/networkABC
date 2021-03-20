220d40b8cbe3e50c468f26e97f9dbc6f5837e60e
Frederic Bertrand <frederic.bertrand@utt.fr>
2021-03-20 21:55:10 +0100

    svg logo and suggests rmarkdown for vignette build

--- genlogo.R
+++ genlogo.R
@@ -36,3 +36,22 @@ sticker(
   h_color = "grey",
   filename = "man/figures/logo.png"
 )
+
+
+sticker(
+  r,
+  package = "networkABC",
+  p_size = 7,
+  s_x = .98,
+  s_y = 0.72,
+  s_width = 1.7,
+  s_height = 1.3,
+  p_x = 1,
+  p_y = 1.3,
+  url = "https://cran.r-project.org/package=networkABC",
+  u_color = "white",
+  u_size = 1.05,
+  h_fill = "black",
+  h_color = "grey",
+  filename = "man/figures/logo_networkABC.svg"
+)
